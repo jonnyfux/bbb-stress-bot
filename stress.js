5e85ddd0d54cbabdc1a20e245b61304f5fafb9de
jonnyfux <58081454+jonnyfux@users.noreply.github.com>
2021-08-31 14:44:05 +0000

    Update stress.js

--- stress.js
+++ stress.js
@@ -4,11 +4,4 @@ const actions = async page => {
   await bigbluebot.video.join(page);
 };
 
-const options = {
-  host: 'SERVER_PLACEHOLDER', // change to your server
-  secret: 'SECRET_PLACEHOLDER', // change to your secret
-  version: '2.3',
-  room: 'MEETING_PLACEHOLDER', // change to your room
-};
-
-bigbluebot.run(actions, options);
+bigbluebot.run(actions);
