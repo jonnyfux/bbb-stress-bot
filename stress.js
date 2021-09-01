0e73063732ee555b5b25db9871a1fc57917d2de5
jonnyfux <58081454+jonnyfux@users.noreply.github.com>
2021-09-01 13:38:00 +0000

    Update stress.js

--- stress.js
+++ stress.js
@@ -1,7 +1,7 @@
 const bigbluebot = require('bigbluebot');
 
 const actions = async page => {
-    await bigbluebot.audio.modal.microphone(page);
+    await bigbluebot.video.join(page);
 };
 
 bigbluebot.run(actions);
